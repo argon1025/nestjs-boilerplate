74b8f97878616eba952362a2e045c55f0628f094
argon1025 <argon1025@gmail.com>
2024-03-18 21:10:19 +0900

    feat: 환경변수 로드 하도록 구성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const servicePort = configService.getOrThrow<number>('SERVICE_PORT');
+  await app.listen(servicePort);
+
+  Logger.log(`Server is running on: ${await app.getUrl()}, Swagger: ${await app.getUrl()}/api`, 'Bootstrap');
 }
 bootstrap();
